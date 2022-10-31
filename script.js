1c4fc30c881d80b56af975189de6c5a36466c7f6
asmallick1999 <asmallick1999@gmail.com>
2022-10-31 13:24:22 +0530

    More Candidate Add

--- script.js
+++ script.js
@@ -34,6 +34,66 @@ const candidates = [
         "Tag_No": "1363",
         "Marks": "<input type='number' onchange='saveMarksLS(this)' name='1363' step='0.01' min='0' max='10' class='form-control border border-dark' placeholder='Max 10' aria-describedby='basic-addon1' required>",
         "Name": "Suman Kalyan Das "
+    },
+    {
+        "SL": "7",
+        "Tag_No": "2009",
+        "Marks": "<input type='number' onchange='saveMarksLS(this)' name='6357' step='0.01' min='0' max='10' class='form-control border border-dark' placeholder='Max 10' aria-describedby='basic-addon1' required>",
+        "Name": "Amir Sohail"
+    },
+    {
+        "SL": "8",
+        "Tag_No": "4000",
+        "Marks": "<input type='number' onchange='saveMarksLS(this)' name='7659' step='0.01' min='0' max='10' class='form-control border border-dark' placeholder='Max 10' aria-describedby='basic-addon1' required>",
+        "Name": "Sk Hasibul "
+    },
+    {
+        "SL": "9",
+        "Tag_No": "4092",
+        "Marks": "<input type='number' onchange='saveMarksLS(this)' name='7657' step='0.01' min='0' max='10' class='form-control border border-dark' placeholder='Max 10' aria-describedby='basic-addon1' required>",
+        "Name": "Chandan Jana"
+    },
+    {
+        "SL": "10",
+        "Tag_No": "12356",
+        "Marks": "<input type='number' onchange='saveMarksLS(this)' name='61' step='0.01' min='0' max='10' class='form-control border border-dark' placeholder='Max 10' aria-describedby='basic-addon1' required>",
+        "Name": "KUNAL SAHA"
+    },
+    {
+        "SL": "11",
+        "Tag_No": "12429",
+        "Marks": "<input type='number' onchange='saveMarksLS(this)' name='97' step='0.01' min='0' max='10' class='form-control border border-dark' placeholder='Max 10' aria-describedby='basic-addon1' required>",
+        "Name": "SHIBAM DAS"
+    },
+    {
+        "SL": "12",
+        "Tag_No": "13245",
+        "Marks": "<input type='number' onchange='saveMarksLS(this)' name='66' step='0.01' min='0' max='10' class='form-control border border-dark' placeholder='Max 10' aria-describedby='basic-addon1' required>",
+        "Name": "PRASIT DAS"
+    },
+    {
+        "SL": "13",
+        "Tag_No": "13246",
+        "Marks": "<input type='number' onchange='saveMarksLS(this)' name='62' step='0.01' min='0' max='10' class='form-control border border-dark' placeholder='Max 10' aria-describedby='basic-addon1' required>",
+        "Name": "ARNAB BISWAS"
+    },
+    {
+        "SL": "14",
+        "Tag_No": "13249",
+        "Marks": "<input type='number' onchange='saveMarksLS(this)' name='7398' step='0.01' min='0' max='10' class='form-control border border-dark' placeholder='Max 10' aria-describedby='basic-addon1' required>",
+        "Name": "amir sohail"
+    },
+    {
+        "SL": "15",
+        "Tag_No": "13427",
+        "Marks": "<input type='number' onchange='saveMarksLS(this)' name='59' step='0.01' min='0' max='10' class='form-control border border-dark' placeholder='Max 10' aria-describedby='basic-addon1' required>",
+        "Name": "SUPRATIM NATH"
+    },
+    {
+        "SL": "16",
+        "Tag_No": "13467",
+        "Marks": "<input type='number' onchange='saveMarksLS(this)' name='64' step='0.01' min='0' max='10' class='form-control border border-dark' placeholder='Max 10' aria-describedby='basic-addon1' required>",
+        "Name": "PRAMIT DAS"
     }
 ]
 
@@ -64,15 +124,15 @@ function myFunction(){
                             <button onclick = "btnClicked${i.SL}(10)">10</button>
                             <button onclick = "btnClicked${i.SL}(5.5)">5.5</button>
                             <button onclick = "btnClicked${i.SL}(5)">5</button>
-                            <button onclick = "btnClicked${i.SL}(4.5)>4.5</button>
-                            <button onclick = "btnClicked${i.SL}(4)>4</button>
-                            <button onclick = "btnClicked${i.SL}(3.5)>3.5</button>
-                            <button onclick = "btnClicked${i.SL}(3)>3</button>
-                            <button onclick = "btnClicked${i.SL}(2.5)>2.5</button>
-                            <button onclick = "btnClicked${i.SL}(2)>2</button>
-                            <button onclick = "btnClicked${i.SL}(1.5)>1.5</button>
-                            <button onclick = "btnClicked${i.SL}(1)>1</button>
-                            <button onclick = "btnClicked${i.SL}(0)>0</button>
+                            <button onclick = "btnClicked${i.SL}(4.5)">4.5</button>
+                            <button onclick = "btnClicked${i.SL}(4)">4</button>
+                            <button onclick = "btnClicked${i.SL}(3.5)">3.5</button>
+                            <button onclick = "btnClicked${i.SL}(3)">3</button>
+                            <button onclick = "btnClicked${i.SL}(2.5)">2.5</button>
+                            <button onclick = "btnClicked${i.SL}(2)">2</button>
+                            <button onclick = "btnClicked${i.SL}(1.5)">1.5</button>
+                            <button onclick = "btnClicked${i.SL}(1)">1</button>
+                            <button onclick = "btnClicked${i.SL}(0)">0</button>
                         </div>  `
     
     }
@@ -131,34 +191,34 @@ function btnClicked17(val){
     document.getElementsByTagName('input')[16].value = val;
 }
 function btnClicked18(val){
-    document.getElementsByTagName('input')[16].value = val;
+    document.getElementsByTagName('input')[17].value = val;
 }
 function btnClicked19(val){
-    document.getElementsByTagName('input')[16].value = val;
+    document.getElementsByTagName('input')[18].value = val;
 }
 function btnClicked20(val){
-    document.getElementsByTagName('input')[16].value = val;
+    document.getElementsByTagName('input')[19].value = val;
 }
 function btnClicked21(val){
-    document.getElementsByTagName('input')[16].value = val;
+    document.getElementsByTagName('input')[20].value = val;
 }
 function btnClicked22(val){
-    document.getElementsByTagName('input')[16].value = val;
+    document.getElementsByTagName('input')[21].value = val;
 }
 function btnClicked23(val){
-    document.getElementsByTagName('input')[16].value = val;
+    document.getElementsByTagName('input')[22].value = val;
 }
 function btnClicked24(val){
-    document.getElementsByTagName('input')[16].value = val;
+    document.getElementsByTagName('input')[23].value = val;
 }
 function btnClicked25(val){
-    document.getElementsByTagName('input')[16].value = val;
+    document.getElementsByTagName('input')[24].value = val;
 }
 function btnClicked26(val){
-    document.getElementsByTagName('input')[16].value = val;
+    document.getElementsByTagName('input')[25].value = val;
 }
 function btnClicked27(val){
-    document.getElementsByTagName('input')[16].value = val;
+    document.getElementsByTagName('input')[26].value = val;
 }
 
 
